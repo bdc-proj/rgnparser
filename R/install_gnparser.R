f08fd46b50d2c721c03ac1237e26fa07a4fad5db
Karlo Guidoni <kguidonimartins@gmail.com>
2022-07-11 15:33:16 -0300

    hotfix grep for windows release
    Related to: https://github.com/brunobrr/bdc/issues/230

--- R/install_gnparser.R
+++ R/install_gnparser.R
@@ -76,7 +76,7 @@ install_gnparser = function(version = 'latest', force = FALSE) {
   }
 
   files = if (is_windows()) {
-    download_file('win', '.zip')
+    download_file('win-', '.zip')
   } else if (is_macos()) {
     download_file("mac", '.tar.gz')
   } else {
